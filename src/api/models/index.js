d6f4d48da73c29826bb1d38ed6d835ca545fd2ed
maheshChudasama98 <124432658+maheshChudasama98@users.noreply.github.com>
2023-11-11 13:06:24 +0530

    basic set up comleted.

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -4,19 +4,11 @@ const db = {}
 
 // All modal include here  
 db.UserTypesModel = require("./UserTypes.model")(sequelize, DataTypes)
-db.AccountTypesModel = require("./AccountTypes.model")(sequelize, DataTypes)
-
 db.UserModel = require("./Users.model")(sequelize, DataTypes)
-db.AccountsModel = require("./Accounts.model")(sequelize, DataTypes)
+
 
 // Join Models here
 db.UserTypesModel.hasOne(db.UserModel, { foreignKey: 'userTypeId' });
 db.UserModel.belongsTo(db.UserTypesModel, { foreignKey: 'userTypeId' });
-
-db.AccountTypesModel.hasOne(db.AccountsModel, { foreignKey: 'accountTypeId'});
-db.AccountsModel.belongsTo(db.AccountTypesModel, { foreignKey: 'accountTypeId'});
-
-db.UserModel.hasOne(db.AccountsModel, { foreignKey: 'userId'});
-db.AccountsModel.belongsTo(db.UserModel, { foreignKey: 'userId'});
 
 module.exports = db
