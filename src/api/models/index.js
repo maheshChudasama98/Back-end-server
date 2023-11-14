6c98c266a91fa59f483b7d3c6046eaded664af81
maheshChudasama98 <124432658+maheshChudasama98@users.noreply.github.com>
2023-11-14 15:03:34 +0530

    join models

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -8,6 +8,11 @@ db.UserModel = require("./Users.model")(sequelize, DataTypes)
 db.EducationModel = require("./Education.model")(sequelize, DataTypes)
 db.ExperienceModel = require("./Experience.model")(sequelize, DataTypes)
 db.ProjectsModel = require("./Projects.model")(sequelize, DataTypes)
+db.CompaniesModel = require("./Companies.model")(sequelize, DataTypes)
+
+db.TechnologyModel = require("./Technology.model")(sequelize, DataTypes)
+db.CertificatesModel = require("./Certificates.model")(sequelize, DataTypes)
+db.LanguagesModel = require("./Languages.model")(sequelize, DataTypes)
 
 
 // Join Models here 
@@ -22,6 +27,18 @@ db.ExperienceModel.belongsTo(db.UserModel, { foreignKey: 'createdByUserId' });
 
 db.UserModel.hasMany(db.ProjectsModel, { foreignKey: 'createdByUserId' }); // one to many
 db.ProjectsModel.belongsTo(db.UserModel, { foreignKey: 'createdByUserId' });
+
+db.UserModel.hasMany(db.CompaniesModel, { foreignKey: 'createdByUserId' }); // one to many
+db.CompaniesModel.belongsTo(db.UserModel, { foreignKey: 'createdByUserId' });
+
+db.UserModel.hasMany(db.TechnologyModel, { foreignKey: 'createdByUserId' }); // one to many
+db.TechnologyModel.belongsTo(db.UserModel, { foreignKey: 'createdByUserId' });
+
+db.UserModel.hasMany(db.CertificatesModel, { foreignKey: 'createdByUserId' }); // one to many
+db.CertificatesModel.belongsTo(db.UserModel, { foreignKey: 'createdByUserId' });
+
+db.UserModel.hasMany(db.LanguagesModel, { foreignKey: 'createdByUserId' }); // one to many
+db.LanguagesModel.belongsTo(db.UserModel, { foreignKey: 'createdByUserId' });
 
 
 module.exports = db
